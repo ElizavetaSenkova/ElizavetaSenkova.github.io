4b7bbf555162b40fa23f356b025a052ed73e17ea
ElizavetaSenkova <lizok9910@mail.ru>
2019-11-01 12:59:48 +0300

    new

---
+++ public/js/common.js
@@ -0,0 +1,59 @@
+$(function() {
+
+	$(".start-middle__two").fadeIn(1100);
+	$(".start-middle__one").fadeIn(1900);
+
+	// page-header
+	$(".page-header__nav button").click(function(){
+		$(this).toggleClass("selected");
+		$(this).prev('.page-header__nav-link').toggleClass("active");
+		$(this).parent().siblings().find("button").removeClass("selected");
+		$(this).parent().siblings().find('.page-header__nav-link').removeClass("active");
+  });
+  
+             // mobile
+  $(".menu-toggle").click(function(){
+		$(this).toggleClass("active");
+		$(this).toggleClass("close");
+		if ( $(".page-header").is(":visible") ){
+			$(".page-header").fadeOut();
+			$(".overlay").css('display','none');
+			$("body").css('overflow-y','auto');
+		} else {
+			$(".page-header").fadeIn();
+			$(".overlay").css('display','block');
+			$("body").css('overflow-y','hidden');
+		}
+	});
+
+	$(".overlay").click(function(){
+		$(".menu-toggle").toggleClass("active");
+		$(".menu-toggle").toggleClass('close');
+		if ( $(".page-header").is(":visible") ){
+			$(".page-header").fadeOut();
+			$(".overlay").css('display','none');
+			$("body").css('overflow-y','auto');
+		} else {
+			$(".page-header").fadeIn();
+			$(".overlay").css('display','block');
+			$("body").css('overflow-y','hidden');
+		}
+	});
+  // page-header
+
+
+	// burger
+
+	$('.menu-toggle.burger').each(function(){
+		$(this).empty().append('<div class="box"></div>');
+		var i = 0;
+		var rect_classes = ['top','middle','bottom'];
+		while (i < 3) {
+			$(this).find('.box').append('<div class="rect rect-'+ rect_classes[i] +'"></div>');
+			i++;
+		}
+	});
+	
+	// burger
+		  
+});
