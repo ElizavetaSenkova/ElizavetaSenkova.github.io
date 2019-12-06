1402fcdedccadae9a6337c6c523cd2da853ef3c1
Liza <lizok9910@mail.ru>
2019-12-06 13:38:34 +0300

    Добавляет анимацию

--- test/js/common.js
+++ test/js/common.js
@@ -41,5 +41,25 @@ $(function() {
 			$("body").css('overflow-y','hidden');
 		}
 	});
+		
+		var show = true;
+    var countbox = ".course";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.course-list li span').css('opacity', '1');
+            $('.course-list li span').spincrement({
+                thousandSeparator: "",
+                duration: 2000,
+            });
+             
+            show = false;
+        }
+    });
 
 });
