4c07dae7bfde907323010f3f982f362d6f88fae9
ElizavetaSenkova <lizok9910@mail.ru>
2020-05-06 18:08:19 +0300

    new project

--- CasinoGuru/js/common.js
+++ CasinoGuru/js/common.js
@@ -21,4 +21,69 @@ $(function() {
   magicGrid.listen();
   // blog-grid
 
+
+  // popular-slider
+
+	$("#popular-slider").slick({
+		infinite: true,
+		slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    nextArrow: '<i class="popular-arrow-right fas fa-chevron-right"></i>',
+    prevArrow: '<i class="popular-arrow-left fas fa-chevron-left"></i>',
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					arrows: true,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				}
+			},
+		]
+	});
+
+  // popular-slider
+  
+  // online-slider
+
+	$("#online-slider").slick({
+		infinite: true,
+		slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    nextArrow: '<i class="online-arrow-right fas fa-chevron-right"></i>',
+    prevArrow: '<i class="online-arrow-left fas fa-chevron-left"></i>',
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					arrows: true,
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+				}
+			},
+		]
+	});
+
+	// online-slider
+
 });
